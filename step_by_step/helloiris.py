0ddfa57e28c493eaf9c4d7ed922008b2df00d1f0
zed <zed@MacBookPro.lan>
2025-08-27 01:18:00 -0400

    changes to helloiris

--- step_by_step/helloiris.py
+++ step_by_step/helloiris.py
@@ -26,9 +26,9 @@ url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
 
-
-# exploratory data summary
 """
+# exploratory data summary
+
 # dimensions of dataset in (rows, columns) or (instances,attributes) in ML jargon
 print(dataset.shape)
 # head of data
@@ -50,3 +50,9 @@ plt.show()
 
 
 #split data into training- and validation-data
+array = dataset.values
+X = array[:,0:4]
+y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=.20,random_state=1)
+print(X)
+print(y)
